99b240a5f7025c878c80a457de53ba61d4c397d6
Arthur Bellal <arthur.bellal@datadoghq.com>
2019-01-24 15:23:42 +0100

    correctly init the dogstatsd server with aggregator.GetBufferedChannels everywhere

--- cmd/dogstatsd/main.go
+++ cmd/dogstatsd/main.go
@@ -187,7 +187,7 @@ func start(cmd *cobra.Command, args []string) error {
 	}
 
 	aggregatorInstance := aggregator.InitAggregator(s, hname, "agent")
-	statsd, err := dogstatsd.NewServer(aggregatorInstance.GetChannels())
+	statsd, err := dogstatsd.NewServer(aggregatorInstance.GetBufferedChannels())
 	if err != nil {
 		log.Criticalf("Unable to start dogstatsd: %s", err)
 		return nil
